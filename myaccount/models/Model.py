8f008adfc328bf607906eee19d32798792b9141f
Sven Wong <zhuce@wyl.im>
2016-06-15 17:14:01 +0800

    把表名都改成了小写

--- myaccount/models/Model.py
+++ myaccount/models/Model.py
@@ -6,11 +6,11 @@ from myaccount import db
 
 
 class FinanceUsers(db.Model):
-	__tablename__='FINANCE_USERS'
-	id = db.Column('ID',db.Integer, primary_key=True)
-	loginId = db.Column("LOGIN_ID",db.String(30))
-	username = db.Column('USER_NAME',db.String(10))
-	password = db.Column('PASSWORD',db.String(64))
+	__tablename__='finance_users'
+	id = db.Column('id',db.Integer, primary_key=True)
+	loginId = db.Column("login_id",db.String(30))
+	username = db.Column('user_name',db.String(10))
+	password = db.Column('password',db.String(64))
 	books = db.relationship('FinanceBook', backref='finUser',lazy='dynamic')
 	userTags = db.relationship('FinanceTags', backref='craeteUser',lazy='dynamic')
 	userContents = db.relationship('FinanceContent', backref='cntUser',lazy='dynamic')
@@ -23,24 +23,26 @@ class FinanceUsers(db.Model):
 		return '<FinanceUsers %r>' % vars(self)
 
 class FinanceBook(db.Model):
-	__tablename__='FINANCE_BOOK'
-	id = db.Column("ID",db.Integer, primary_key=True)
-	bookName = db.Column('BOOK_NAME',db.String(20))
+	__tablename__='finance_book'
+	id = db.Column("id",db.Integer, primary_key=True)
+	bookName = db.Column('book_name',db.String(20))
+	isDefault = db.Column('is_defalut',db.String(2))
 	finContents = db.relationship('FinanceContent', backref='finBook',lazy='dynamic')
-	userId = db.Column('USER_ID',db.Integer,db.ForeignKey('FINANCE_USERS.ID'))
+	userId = db.Column('user_id',db.Integer,db.ForeignKey('finance_users.id'))
 
-	def __init__(self,bookName,userId):
+	def __init__(self,bookName,userId,isDefault):
 		self.bookName = bookName
 		self.userId = userId
+		self.isDefault = isDefault
 
 	def __repr__(self):
-		return '<FinanceUsers %r>' % vars(self)
+		return '<financeusers %r>' % vars(self)
 
 class FinanceTagsType(db.Model):
-	__tablename__='FINANCE_TAGS_TYPE'
+	__tablename__='finance_tags_type'
 
-	tagTypeId = db.Column('TAG_TYPE_ID',db.String(10),primary_key=True)
-	description = db.Column('DESCRIPTION',db.String(10))
+	tagTypeId = db.Column('tag_type_id',db.String(10),primary_key=True)
+	description = db.Column('description',db.String(10))
 	tagTypeChildren = db.relationship('FinanceTags', backref='tagTypeEntity',lazy='dynamic')
 	def __init__(self,tagTypeId, description):
 		self.tagTypeId = tagTypeId
@@ -51,12 +53,12 @@ class FinanceTagsType(db.Model):
 
 
 class FinanceTags(db.Model):
-	__tablename__='FINANCE_TAGS'
-	id = db.Column("ID",db.Integer, primary_key=True)
-	tagType = db.Column('TAG_TYPE',db.String(10),db.ForeignKey('FINANCE_TAGS_TYPE.TAG_TYPE_ID'))
-	tagName = db.Column('TAG_NAME',db.String(10))
-	tagComments = db.Column('TAG_COMMENTS',db.String(50))
-	userId = db.Column('USER_ID',db.Integer,db.ForeignKey('FINANCE_USERS.ID'))
+	__tablename__='finance_tags'
+	id = db.Column("id",db.Integer, primary_key=True)
+	tagType = db.Column('tag_type',db.String(10),db.ForeignKey('finance_tags_type.tag_type_id'))
+	tagName = db.Column('tag_name',db.String(10))
+	tagComments = db.Column('tag_comments',db.String(50))
+	userId = db.Column('user_id',db.Integer,db.ForeignKey('finance_users.id'))
 	contentTags = db.relationship('ContantTagAssoc', backref='FinTag',lazy='dynamic')
 	def __init__(self,tagType, tagName, tagComments,userId):
 		self.tagType = tagType
@@ -68,15 +70,15 @@ class FinanceTags(db.Model):
 		return '<FINANCE_TAGS %r>' % vars(self)
 
 class FinanceContent(db.Model):
-	__tablename__='FINANCE_CONTENT'
-	id = db.Column("ID",db.Integer, primary_key=True)
-	amount = db.Column('AMOUNT',db.Float)
-	inOut = db.Column('INOUT',db.String(50))
-	craetedDate = db.Column('CREATED_DATE',db.DateTime)
-	billingDate = db.Column('BILLING_DATE',db.Date)
-	comments = db.Column('COMMNETS',db.String(50))
-	bookId = db.Column('BOOK_ID',db.Integer,db.ForeignKey('FINANCE_BOOK.ID'))
-	userId = db.Column('USER_ID',db.Integer,db.ForeignKey('FINANCE_USERS.ID'))
+	__tablename__='finance_content'
+	id = db.Column("id",db.Integer, primary_key=True)
+	amount = db.Column('amount',db.Float)
+	inOut = db.Column('inout',db.String(50))
+	craetedDate = db.Column('created_date',db.DateTime)
+	billingDate = db.Column('billing_date',db.Date)
+	comments = db.Column('comments',db.String(50))
+	bookId = db.Column('book_id',db.Integer,db.ForeignKey('finance_book.id'))
+	userId = db.Column('user_id',db.Integer,db.ForeignKey('finance_users.id'))
 	contentTags = db.relationship('ContantTagAssoc', backref='FinContent',lazy='joined')
 
 	def __init__(self,inOut,amount, billingDate,comments, bookId,userId):
@@ -88,16 +90,16 @@ class FinanceContent(db.Model):
 		self.userId = userId
 
 	def __repr__(self):
-		return '<FinanceContent %r>' % vars(self)
+		return '<financecontent %r>' % vars(self)
 
 class ContantTagAssoc(db.Model):
-	__tablename__='CONTENT_TAGS_ASSOC'
-	contentId = db.Column('CONTENT_ID',db.Integer,db.ForeignKey('FINANCE_CONTENT.ID'),primary_key=True)
-	tagId = db.Column('TAG_ID',db.Integer,db.ForeignKey('FINANCE_TAGS.ID'),primary_key=True)
+	__tablename__='content_tags_assoc'
+	contentId = db.Column('content_id',db.Integer,db.ForeignKey('finance_content.id'),primary_key=True)
+	tagId = db.Column('tag_id',db.Integer,db.ForeignKey('finance_tags.id'),primary_key=True)
 
 	def __init__(self,contentId, tagId):
 		self.contentId = contentId
 		self.tagId = tagId
 
 	def __repr__(self):
-		return '<ContantTagAssoc %r>' % vars(self)
+		return '<contanttagassoc %r>' % vars(self)
