a570b58e004dd84fa582c622a1336cb1e5d38080
Sven Wong <zhuce@wyl.im>
2016-07-19 14:24:15 +0800

    update

--- myaccount/view/fincnt.py
+++ myaccount/view/fincnt.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint,render_template,request,redirect, url_for, session,flash,jsonify
 from sqlalchemy import desc,and_
-from myaccount.models import FinanceContent,ContantTagAssoc
+from myaccount.models import FinanceContent,ContantTagAssoc,FinanceTags,FinanceTagsType
 from myaccount import db
 import time
 
@@ -40,11 +40,35 @@ def loadCurrentDateBill():
 	return render_template("currentDateBill.html",currentDateBills=currentDateBills)
 
 #查询所有账单，实际上默认查询3天内的
-@fincontent.route("/allData",methods=["GET"])
+@fincontent.route("/allData",methods=["GET","POST"])
 def allData():
 	userId = session.get("userId")
-	cntList = FinanceContent.query.filter_by(userId=userId,bookId=session['defbookId']).order_by(desc(FinanceContent.billingDate)).all()
-	return render_template("contentList.html",cntList=cntList)
+
+	query = FinanceContent.query
+	query = query.filter(FinanceContent.userId==userId)
+	query = query.filter(FinanceContent.bookId==session['defbookId'])
+	cntList = query.order_by(desc(FinanceContent.billingDate)).all()
+
+	tempQuery = ContantTagAssoc.query.join(FinanceContent)
+	tempQuery = tempQuery.filter(ContantTagAssoc.tagId=='3')
+	tempQuery = tempQuery.filter(FinanceContent.id==ContantTagAssoc.contentId)
+	tempQuery = tempQuery.filter(and_(FinanceContent.billingDate >= '2016-05-12',FinanceContent.billingDate <= '2016-05-13'))
+
+	tempList = tempQuery.all()
+	reqTags = request.form.getlist('selectTag')
+	tags = [tag for tag in reqTags if tag!=u'']
+
+	if tags != None and len(tags)>0:
+		print("00000000====")
+	
+	tagTypes = FinanceTags.query.filter_by(userId=session.get("userId")).group_by(FinanceTags.tagType).all()
+	tagListGroup = []
+	for tagTypeEntity in tagTypes:
+		tagList = FinanceTags.query.filter_by(tagType=tagTypeEntity.tagType).all()
+		tagType = FinanceTagsType.query.filter_by(tagTypeId=tagTypeEntity.tagType).first()
+		tagListGroup.append({'tagType':tagType,'tagList':tagList})
+
+	return render_template("contentList.html",cntList=cntList,tagListGroup=tagListGroup,tempList=tempList)
 
 #删除一个账单
 @fincontent.route("/delCnt/<id>",methods=["GET"])
